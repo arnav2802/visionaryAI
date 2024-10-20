3e43aa61484feae1d5ea2c23aacd99fadcd27df3
Chirag <chiragsoni8857@gmail.com>
2024-10-20 12:38:32 +0530

    analytics enabled

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
 import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
 import React from 'react'
+import { Analytics } from "@vercel/analytics/react"
 
 const Layout = ({ children }: { children: React.ReactNode}) => {
   return (
      <main className='root'> 
         <Sidebar />
         <MobileNav />
+        <Analytics />
 
         <div className='root-container'>
             <div className='wrapper'>
