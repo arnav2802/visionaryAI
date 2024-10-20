7e07ee05c00707cdd00698f0f648b9bbde7732ab
Chirag <chiragsoni8857@gmail.com>
2024-10-20 21:28:08 +0530

    create transformationForms

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -3,15 +3,18 @@ import TransformationForm from '@/components/shared/TransformationForm';
 import { transformationTypes } from '@/constants'
 import { getUserById } from '@/lib/actions/user.action';
 import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/dist/server/api-utils';
+import { redirect } from 'next/navigation';
 
 const AddTransformationTypePage = async ({ params: {type}}: SearchParamProps) => {
-  const transformation = transformationTypes[type];
   const { userId } = auth();
+  const transformation = transformationTypes[type];
 
-  if(!userId) redirect('/sign-in')
+  if(!userId) {
+    return redirect('/sign-in');
+  }
 
   const user = await getUserById(userId);
+
   return (
     <>
       <Header
@@ -19,12 +22,14 @@ const AddTransformationTypePage = async ({ params: {type}}: SearchParamProps) =>
       subtitle={transformation.subTitle}
       />
 
-      <TransformationForm 
-        action ="Add"
-        userId={user._id}
-        type={transformation.type as TransformationTypeKey}
-        creditBalance={user.creditBalance}
-      />
+      <section className="mt-10">
+        <TransformationForm 
+          action="Add"
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
     </>
   )
 }
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -4,6 +4,15 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
+
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -15,10 +24,13 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { aspectRatioOptions, defaultValues } from "@/constants"
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
 import { title } from "process"
+import { CustomField } from "./CustomField"
+import { use, useState } from "react"
+import { AspectRatioKey } from "@/lib/utils"
  
-const formSchema = z.object({
+export const formSchema = z.object({
     title: z.string(),
     aspectRatio: z.string().optional(),
     color: z.string().optional(),
@@ -26,14 +38,21 @@ const formSchema = z.object({
     publicId: z.string(),
 })
 
-const TransformationForm = ({ action, data = null}: TransformationFormProps) => {
-    const initialValues = data && action === 'Update' ? {
-        title: data?.title,
-        aspectRatio: data?.aspectRatio,
-        color: data?.color,
-        prompt: data?.prompt,
-        publicId: data?.publicId,
-    }: defaultValues
+const TransformationForm = ({ action, data = null, userId, type, creditBalance, config = null }: TransformationFormProps) => {
+  const transformationType = transformationTypes[type];
+  const [image, setImage] = useState(data)
+  const [newTransformation, setNewTransformation] = useState<Transformations | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isTransforming, seiIsTransforming] = useState(false);
+  const [transformationConfig, setTransformationConfig] = useState(config)
+
+  const initialValues = data && action === 'Update' ? {
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  }: defaultValues
     // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -44,26 +63,118 @@ const TransformationForm = ({ action, data = null}: TransformationFormProps) => 
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
   }
+
+  const onSelectFieldHandler = (value: string, onChangeField: (value: string) => void) => {
+
+  }
+
+  const onInputChangeHandler = (fieldName: string, value: string, type: string, onChangeField: (value: string) => void) => {
+
+  }
+
+  const onTransformHandler = () => {}
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        <CustomField 
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({ field }) => <Input {...field}
+          className="input-field" />}
         />
-        <Button type="submit">Submit</Button>
+
+        {type === 'fill' && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({ field }) => (
+              <Select
+                onValueChange={(value) => onSelectFieldHandler(value, field.onChange)}
+              >
+                <SelectTrigger className="select-field">
+                  <SelectValue placeholder="Select size" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Object.keys(aspectRatioOptions).map((key) => (
+                    <SelectItem key={key} value={key} className="select-item">
+                      {aspectRatioOptions[key as AspectRatioKey].label}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
+
+        {(type === 'remove' || type ==='recolor') && (
+          <div className="prompt-field">
+            <CustomField
+              control={form.control}
+              name="prompt"
+              formLabel={
+                type === 'remove' ? 'Object to remove' : 'Object to recolor'
+              }
+              className="w-full"
+              render={(({ field }) => (
+                <Input 
+                  value={field.value}
+                  className="input-field"
+                  onChange={(e) => onInputChangeHandler(
+                    'prompt',
+                    e.target.value,
+                    type,
+                    field.onChange
+                  )}
+                />
+              ))}
+            />
+
+            {type === 'recolor' && (
+              <CustomField
+                control={form.control}
+                name="color"
+                formLabel="Replacement Color"
+                className="w-full"
+                render={({ field }) => (
+                  <Input 
+                    value={field.value}
+                    className="input-field"
+                    onChange={(e) => onInputChangeHandler(
+                      'color',
+                      e.target.value,
+                      'recolor',
+                      field.onChange
+                    )}
+                  />
+                )}
+              />
+            )}
+          </div>
+        )}
+
+        <div className="flex flex-col gap-4">
+          <Button 
+            type="button"
+            className="submit-button capitalize"
+            disabled={isTransforming || newTransformation === null}
+            onClick={onTransformHandler}
+          >
+            {isTransforming ? 'Transforming...' : 'Apply Transformation'}
+          </Button>
+        </div>
+
+        <Button 
+          type="submit"
+          className="submit-button capitalize"
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? 'Submitting...' : 'Save Image'}
+        </Button>
       </form>
     </Form>
   )
