de668a34926f02c52950022552a3b3fa5b896bd2
arnav2802 <arnav171103@gmail.com>
2024-10-21 19:28:19 +0530

    input udadi

--- types/index.d.ts
+++ types/index.d.ts
@@ -5,8 +5,8 @@ declare type Createuserentry = {
     clerkId: string;
     email: string;
     username: string | null;
-    firstName: string;
-    lastName: string;
+    firstName: string | null;
+    lastName: string | null;
     photo: string;
   };
   
