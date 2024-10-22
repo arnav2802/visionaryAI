c91ee1774d46daf88c2349e1378338cc4b5d06a7
arnav2802 <arnav171103@gmail.com>
2024-10-22 16:23:03 +0530

    transformation

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
   return (
     <aside className='sidebar'>
         <div className='flex size-full flex-col gap-4'>
-            <Link href='/sign-up' className='sidebar-logo'>
+            <Link href='/' className='sidebar-logo'>
                 <Image src="/assets/images/logo-text.svg" alt='logo' width={180} height={28}/>
             </Link>
 
