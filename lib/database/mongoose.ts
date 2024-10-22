dd2863dfd85b2233b594e8ade055a77c0e7ced91
arnav2802 <arnav171103@gmail.com>
2024-10-22 08:07:41 +0530

    input udadi

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,33 +1,34 @@
-import mongoose, { Mongoose } from "mongoose";
+import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL!;
+const MONGODB_URL = process.env.MONGODB_URL;
 
-interface MongooseConn {
+interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
+  
 }
 
-let cached: MongooseConn = (global as any).mongoose;
+let cached: MongooseConnection = (global as any).mongoose
 
-if (!cached) {
-  cached = (global as any).mongoose = {
-    conn: null,
-    promise: null,
-  };
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
-export const connect = async () => {
-  if (cached.conn) return cached.conn;
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "VisionaryAI",
-      bufferCommands: false,
-      connectTimeoutMS: 30000,
-    });
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'VisionaryAI', bufferCommands: false 
+    })
+    
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-};
+}
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/client"]);
+const isProtectedRoute = createRouteMatcher(["/api/webhooks"]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
