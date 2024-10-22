1fea20942fa70cabf27ffb2d73d38ac9273cc2d6
arnav2802 <arnav171103@gmail.com>
2024-10-22 16:05:26 +0530

    transformation

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -45,7 +45,7 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config)
-  const [ startTransition] = useTransition()
+  // const [ startTransition] = useTransition()
   const router = useRouter()
 
   const initialValues = data && action === 'Update' ? {
@@ -165,9 +165,9 @@ const TransformationForm = ({ action, data = null, userId, type, creditBalance, 
 
     setNewTransformation(null)
     
-    startTransition(async () => {
-      await updateCredits(userId, creditFee)  /*mein yhaa hu maalik */
-    })
+    // startTransition(async () => {
+    //   await updateCredits(userId, creditFee)  /*mein yhaa hu maalik */
+    // })
   }
 
   useEffect(() => {
