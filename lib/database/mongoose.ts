d28ba1a66333a1e9045b5644e68b9d8ffa960c30
arnav2802 <arnav171103@gmail.com>
2024-10-22 07:58:11 +0530

    input udadi

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,34 +1,33 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL!;
 
-interface MongooseConnection {
+interface MongooseConn {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
-  
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+let cached: MongooseConn = (global as any).mongoose;
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
-export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+export const connect = async () => {
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'VisionaryAI', bufferCommands: false 
-    })
-    
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "VisionaryAI",
+      bufferCommands: false,
+      connectTimeoutMS: 30000,
+    });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
