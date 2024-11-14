7e36adb155758aa1737c6fbc372bcf9cc4d902a9
arnav2802 <arnav171103@gmail.com>
2024-11-14 14:51:47 +0530

    deprecated keyworrds

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -62,7 +62,7 @@ const Sidebar = () => {
                                 )
                             })} 
                         <li className='flex-center cursor-pointer gap-2 p-4'>
-                            <UserButton afterSignOutUrl='/' showName />                  /* afterSignOutUrl */
+                            <UserButton afterSignOutUrl='/' showName />                  
                         </li>
                     </ul>
                 </SignedIn>
